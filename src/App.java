798ac1c8aaa1a9ef7c8e1b2436cfe87aa11b9e10
Jeansilva05 <jeanprg074@gmail.com>
2024-11-19 18:04:20 -0300

    :sparkles: Adicionando gitignore e txt

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Genero genero = new Genero(1, "Terror", "A");
+        genero.inserir();
     }
 }
