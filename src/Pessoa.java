4d0fc0bc8191c8d6f559dc81065df8f41bf23113
Jeansilva05 <jeanprg074@gmail.com>
2024-11-19 21:22:22 -0300

    :sparkles: Adicionando classe abstrata Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public abstract class Pessoa {
+    private String cpf;
+    private String nome;
+    private String email;
+
+    public void falarNome(){
+        System.out.println(this.nome);
+    }
+}
